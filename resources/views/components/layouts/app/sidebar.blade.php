f8ca009bfbf1e668b50035edcaa89d2ec88dbe9b
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-07-31 20:17:06 +0100

    ok

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -4,13 +4,19 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
-
+    <div class="w-64 float-right fixed top-0 right-0 z-0">
+        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
+            <flux:radio value="light" icon="sun"></flux:radio>
+            <flux:radio value="dark" icon="moon"></flux:radio>
+            <flux:radio value="system" icon="computer-desktop"></flux:radio>
+        </flux:radio.group>
+    </div>
         <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
             <div class="lg:flex lg:justify-between lg:gap-4">
                 <header class="lg:sticky lg:top-0 lg:flex lg:top-0 lg:max-h-screen lg:w-1/3 lg:flex-col lg:justify-between lg:py-24">
                     <div>
-                        <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl"><a href="/">Kevin Wilson</a></h1>
-                        <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Multi disciplined IT Professional</h2>
+                        <h1 class="text-4xl font-bold tracking-tight sm:text-5xl dark:text-slate-300 text-slate-600"><a href="/">Kevin Wilson</a></h1>
+                        <h2 class="mt-3 text-lg font-medium tracking-tight dark:text-slate-50 text-slate-600" sm:text-xl ">Multi disciplined IT Professional</h2>
                         <p class="mt-4 max-w-xs leading-normal">Blah blah blah blah blah.</p>
                         <nav class="nav hidden lg:block" aria-label="In-page jump links">
                             <ul class="mt-16 w-max">
@@ -75,7 +81,7 @@
                 </header>
                 <main id="content" class="py-12 lg:pt-24 lg:w-2/3">
                     <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me">
-                        <h3 class="text-2xl font-bold py-4">About me</h3>
+                        <h3 class="text-2xl font-bold py-4 dark:text-slate-300 text-slate-600">About me</h3>
                         <div>
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi porro ducimus, amet totam labore error aliquam sed laboriosam magnam? Animi sequi suscipit ratione rerum dicta repellat recusandae accusantium cum ducimus.
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quidem impedit rerum consectetur porro perspiciatis non repellat ea, quos cumque officia mollitia dignissimos provident ducimus perferendis placeat ipsum, ut recusandae!
@@ -83,7 +89,7 @@
                     </section>
 
                     <section id="experience" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="experience">
-                        <h3 class="text-2xl font-bold mb-4">Experience</h3>
+                        <h3 class="text-2xl font-bold mb-4 dark:text-slate-300 text-slate-600">Experience</h3>
                         <ul>
                             <li class="mt-12">
                                 <div class="flex">
@@ -138,26 +144,110 @@
                         </ul>
                     </section>
 
-                    <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="experience">
-                        <h3 class="text-2xl font-bold">Coding Projects</h3>
+                    <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="projects">
+                        <h3 class="text-2xl font-bold dark:text-slate-300 text-slate-600">Coding Projects</h3>
+                        <h4 class="text-1xl text-left my-4 font-bold dark:text-slate-300 text-slate-600">Website / Web Application Development</h4>
                         <div class="mt-8">
-                            <div>Project 1</div>
-                            <div>Project 2</div>
-                            <div>Project 2</div>
-                            <div>Project 2</div>
-                            <div>Project 2</div>
-                            <div>Project 2</div>
+                            <a href="https://claires-recipes.uk" target="_blank" aria-label="claires recipes">
+                                <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
+                                    <div class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/claires-recipes.PNG') }}" alt="claires-recipes.uk">
+                                        </div>
+                                        <div>
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Claires Recipes</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <div class="flex mt-4">
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    PHP
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Laravel
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Livewire
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    AlpineJS
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Bootstrap
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="mt-8">
+                            <a target="_blank" href="https://kevkb.kevinlwilson.co.uk" aria-label="knowledge base">
+                                <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
+                                    <div class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/kb.PNG') }}" alt="claires-recipes.uk">
+                                        </div>
+                                        <div>
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Knowledge Base</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <div class="flex mt-4">
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    PHP
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Laravel
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    VueJS
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Bootstrap
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="mt-8">
+                            <a href="https://danska-property.uk" target="_blank" aria-label="danska property">
+                                <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
+                                    <div class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/claires-recipes.PNG') }}" alt="claires-recipes.uk">
+                                        </div>
+                                        <div>
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Danska Property Services</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <div class="flex mt-4">
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    PHP
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Laravel
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Livewire
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    AlpineJS
+                                                </button>
+                                                <button class="bg-zinc-700 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Bootstrap
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     </section>
                 </main>
             </div>
         </div>
 
-<flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
-                    <flux:radio value="light" icon="sun"></flux:radio>
-                    <flux:radio value="dark" icon="moon"></flux:radio>
-                    <flux:radio value="system" icon="computer-desktop"></flux:radio>
-        </flux:radio.group>
+
         @fluxScripts
     </body>
 </html>
