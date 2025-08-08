721f41bb8cc3b161e86c229ddf58974b17ec5fe0
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-08-08 22:13:54 +0100

    ok

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -369,7 +369,7 @@
                             <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
                                 <div x-data class="flex">
                                         <div class="pr-8">
-                                            <img class="h-32 w-128" src="{{ asset('storage/powershell.jpg') }}" alt="claires-recipes.uk">
+                                            <img class="h-32 w-128" src="{{ asset('storage/PowerShell.webp') }}" alt="claires-recipes.uk">
                                         </div>
                                         <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
                                             <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Modify Active Directory Phone Numbers</h3>
@@ -405,7 +405,7 @@
                             <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
                                 <div x-data class="flex">
                                         <div class="pr-8">
-                                            <img class="h-32 w-128" src="{{ asset('storage/powershell.jpg') }}" alt="claires-recipes.uk">
+                                            <img class="h-32 w-128" src="{{ asset('storage/PowerShell.webp') }}" alt="claires-recipes.uk">
                                         </div>
                                         <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
                                             <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">HTML Server Reports</h3>
@@ -444,7 +444,7 @@
                             <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md w-full">
                                 <div x-data class="flex">
                                         <div class="pr-8">
-                                            <img class="h-32 w-128" src="{{ asset('storage/powershell.jpg') }}" alt="powershell">
+                                            <img class="h-32 w-128" src="{{ asset('storage/PowerShell.webp') }}" alt="powershell">
                                         </div>
                                         <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
                                             <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Get Software Uninstall String From the Registry</h3>
@@ -480,7 +480,124 @@
                                     </div>
 
                             </div>
-                    </section>
+
+                           <h4 class="text-1xl text-left my-4 font-bold dark:text-slate-300 text-slate-600">Python</h4>
+                            <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
+                                <div x-data class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/python.jpeg') }}" alt="claires-recipes.uk">
+                                        </div>
+                                        <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Modify Active Directory Phone Numbers</h3>
+                                            <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <button x-on:click="isModalOpen = true" class="cursor-pointer hover:bg-zinc-900 bg-zinc-700 mt-2 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Show Code
+                                            </button>
+                                            <div
+                                            class="modal"
+                                            role="dialog"
+                                            tabindex="-1"
+                                            x-show="isModalOpen"
+                                            x-on:click.away="isModalOpen = false"
+                                            x-cloak
+                                            x-transition
+                                            >
+                                                <div class="model-inner w-96 md:w-128 mt-2 overflow-auto">
+                                                    <div class="modal-header">
+
+                                                        <button aria-label="Close" x-on:click="isModalOpen=false">✖</button>
+                                                    </div>
+                                                    <pre class="language-powershell">
+                                                            <code>
+                                                            @include('components.code_snippets.phone')
+                                                            </code>
+                                                    </pre>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+
+                            <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md">
+                                <div x-data class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/python.jpeg') }}" alt="claires-recipes.uk">
+                                        </div>
+                                        <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">HTML Server Reports</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <button x-on:click="isModalOpen = true" class="cursor-pointer hover:bg-zinc-900 bg-zinc-700 mt-2 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Show Code
+                                            </button>
+                                            <div
+                                            class="modal"
+                                            role="dialog"
+                                            tabindex="-1"
+                                            x-show="isModalOpen"
+                                            x-on:click.away="isModalOpen = false"
+                                            x-cloak
+                                            x-transition
+                                            >
+                                                <div class="model-inner w-96 md:w-128 mt-2 bg-white text-black overflow-auto">
+                                                    <div class="modal-header">
+
+                                                        <button aria-label="Close" x-on:click="isModalOpen=false">✖</button>
+                                                    </div>
+                                                    <pre class="language-powershell">
+                                                        <code>
+
+                                                                @include('components.code_snippets.server')
+
+                                                        </code>
+                                                    </pre>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                            </div>
+
+                            <div class="dark:hover:bg-zinc-600 p-4 hover:rounded-md w-full">
+                                <div x-data class="flex">
+                                        <div class="pr-8">
+                                            <img class="h-32 w-128" src="{{ asset('storage/python.jpeg') }}" alt="powershell">
+                                        </div>
+                                        <div x-data="{'isModalOpen': false}" x-on:keydown.escape="isModalOpen=false">
+                                            <h3 class="pb-2 text-xl dark:text-slate-300 text-slate-600">Get Software Uninstall String From the Registry</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quos voluptatem architecto aut incidunt omnis voluptate consequatur numquam odio ipsum, nemo totam, ratione impedit cum molestiae debitis et, laboriosam magnam?</p>
+                                            <button x-on:click="isModalOpen = true" class="cursor-pointer hover:bg-zinc-900 bg-zinc-700 mt-2 mr-1 rounded-xl text-white text-xs font-bold py-1 px-2 rounded">
+                                                    Show Code
+                                            </button>
+                                            <div
+                                            class="modal"
+                                            role="dialog"
+                                            tabindex="-1"
+                                            x-show="isModalOpen"
+                                            x-on:click.away="isModalOpen = false"
+                                            x-cloak
+                                            x-transition
+                                            >
+                                                <div class="model-inner w-96 md:w-128 mt-2 bg-white text-black overflow-auto">
+                                                    <div class="modal-header">
+
+                                                        <button aria-label="Close" x-on:click="isModalOpen=false">✖</button>
+                                                    </div>
+                                                    <pre>
+                                                        <code class="language-powershell">
+
+                                                                @include('components.code_snippets.uninstall')
+
+                                                        </code>
+                                                    </pre>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                            </div>
+
+                        </section>
                 </main>
             </div>
         </div>
