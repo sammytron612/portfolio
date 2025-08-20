4afbbce3fab47c138fa84ce5ca53d68832506fd7
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-08-20 15:09:36 +0100

    ok

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
 
-    <body class="min-h-screen bg-white dark:bg-zinc-800">
+    <body body x-data="{darkMode: true}" :class="{'dark': darkMode === true }"  class="antialiased min-h-screen bg-white dark:bg-zinc-800">
         <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
             <div class="lg:flex lg:justify-between lg:gap-4">
                 <header class="lg:sticky lg:top-0 lg:flex lg:top-0 lg:max-h-screen lg:w-1/3 lg:flex-col lg:justify-between lg:py-24" role="banner">
@@ -77,13 +77,30 @@
                                 </a>
                             </li>
                         </ul>
-                        <nav class="md:mt-4 mt-8 hover:cursor-pointer border-1 dark:border-slate-700 border-slate-200 w-48" aria-label="Theme switcher">
-                            <flux:radio.group x-data="{ value: 'dark' }" variant="segmented" x-model="value">
-                                <flux:radio value="light" icon="sun" aria-label="Switch to light mode"></flux:radio>
-                                <flux:radio value="dark" icon="moon" aria-label="Switch to dark mode" selected></flux:radio>
-                                <flux:radio value="system" icon="computer-desktop" aria-label="Use system theme"></flux:radio>
-                            </flux:radio.group>
-                        </nav>
+                        <button @click="darkMode=!darkMode" type="button" class="ml-8 mt-4 relative inline-flex flex-shrink-0 h-6 mr-5 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer bg-zinc-200 dark:bg-zinc-700 w-11 focus:outline-none focus:ring-2 focus:ring-neutral-700 focus:ring-offset-2" role="switch" aria-checked="false">
+                            <span class="sr-only">Use setting</span>
+                            <span class="relative inline-block w-5 h-5 transition duration-500 ease-in-out transform translate-x-0 bg-white rounded-full shadow pointer-events-none dark:translate-x-5 ring-0">
+                                <span class="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-500 ease-in opacity-100 dark:opacity-0 dark:duration-100 dark:ease-out" aria-hidden="true">
+                                    {{--
+                                    <x-svg class="w-4 h-4 text-neutral-700" svg="sun"/>
+                                    --}}
+                                    <svg  xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun w-4 h-4 text-neutral-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                                        <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+                                    </svg>
+                                </span>
+                                <span class="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity duration-100 ease-out opacity-0 dark:opacity-100 dark:duration-200 dark:ease-in" aria-hidden="true">
+                                    {{--
+                                    <x-svg class="w-4 h-4 text-neutral-700" svg="moon"/>
+                                    --}}
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon w-4 h-4 text-neutral-700" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                                    </svg>
+                                </span>
+                            </span>
+                        </button>
 
                     </div>
                 </header>
